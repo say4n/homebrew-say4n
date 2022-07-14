16bfc9dd66d6eede27ed38fed2376cc6b625780e
Sayan Goswami <goswami.sayan47@gmail.com>
2022-07-15 01:00:53 +0530

    Updates URL.

--- Casks/smartthings-cli.rb
+++ Casks/smartthings-cli.rb
@@ -2,7 +2,7 @@ cask "smartthings-cli" do
   version "1.0.0-beta.10"
   sha256 "7db14bac6f9aee792f53d2046e8ec51555a5bd580998441cba2c4480e9c1450a"
 
-  url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/%40smartthings%2Fcli%40#{version}/smartthings-macos.zip"
+  url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/@smartthings/cli@#{version}/smartthings-macos.zip"
   name "smartthings-cli"
   desc "Samsung SmartThings CLI"
   homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
