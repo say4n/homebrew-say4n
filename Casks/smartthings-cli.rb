b0b3257135ab2b14ad2a35b19b28101f0640b942
Sayan Goswami <goswami.sayan47@gmail.com>
2022-07-15 00:42:02 +0530

    Adds smartthings-cli cask.

---
+++ Casks/smartthings-cli.rb
@@ -0,0 +1,13 @@
+cask "smartthings-cli" do
+    version "1.0.0-beta.10"
+    sha256 ""
+
+    language
+
+    url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/%40smartthings%2Fcli%40{version}/smartthings-macos.zip"
+    name "smartthings-cli"
+    desc "Samsung SmartThings CLI"
+    homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
+
+    binary "smartthings"
+end
