fab8c1aa83907f8fe202d5749363a901650000c4
Sayan Goswami <goswami.sayan47@gmail.com>
2022-07-15 00:58:07 +0530

    Removes language field.

--- Casks/smartthings-cli.rb
+++ Casks/smartthings-cli.rb
@@ -2,8 +2,6 @@ cask "smartthings-cli" do
   version "1.0.0-beta.10"
   sha256 "7db14bac6f9aee792f53d2046e8ec51555a5bd580998441cba2c4480e9c1450a"
 
-  language
-
   url "https://github.com/SmartThingsCommunity/smartthings-cli/releases/download/%40smartthings%2Fcli%40#{version}/smartthings-macos.zip"
   name "smartthings-cli"
   desc "Samsung SmartThings CLI"
